9bd08184ad0c242b0391bd11356e62deee2b08b9
AC <75492066+AparnaChellapilla@users.noreply.github.com>
2021-02-10 09:47:42 -0600

    Initial commit

---
+++ src/test/java/com/tts/oAuthLab/OAuthLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.oAuthLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
